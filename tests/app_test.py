243da480c93d3167d8553c2b54c6a500b6b55e9d
Fedor <f.lushev@yandex.ru>
2023-03-30 15:32:20 +0700

    Добавлен тест для метода add_alarm() класса App

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,8 +1,38 @@
 from classes.app import App
+from classes.tune import Tune
+from classes.alarm import Alarm
+import datetime
 
 
 class TestApp:
+    @staticmethod
+    def create_tune(self):
+        tune = Tune(
+            name='test_tune',
+            path='tunes/test_tune.mp3'
+        )
+        return tune
+
+    @staticmethod
+    def create_alarm(self):
+        tune = self.create_tune()
+        time = datetime.time(hour=8, minute=0)
+
+        alarm = Alarm(
+            name='test_alarm',
+            description='test_description',
+            tune=tune,
+            time=time
+        )
+        return alarm
 
     def test_create_app(self):
         app = App()
         assert app is not None
+
+    def test_app_add_alarm(self):
+        app = App()
+        alarm = self.create_alarm(self)
+        app.add_alarm(alarm)
+        alarms = app.alarms
+        assert alarms is not None
