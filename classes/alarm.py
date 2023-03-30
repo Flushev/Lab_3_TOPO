7f48d059bc0e1bb8c1f367f26dfbb917c0ac5ca8
Fedor <f.lushev@yandex.ru>
2023-03-30 14:25:22 +0700

    Добавлено поле is_active для класса Alarm. Тесты остались актуальными.

--- classes/alarm.py
+++ classes/alarm.py
@@ -7,9 +7,11 @@ class Alarm:
     description: str
     tune: Tune
     time: datetime.time
+    is_active: bool
 
-    def __init__(self, name, description, tune, time):
+    def __init__(self, name, description, tune, time, is_active=False):
         self.name = name
         self.description = description
         self.time = time
         self.tune = tune
+        self.is_active = is_active
