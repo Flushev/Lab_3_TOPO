18b4c19efaa3ad41efdeb56cb64c96de0a33b5a0
Fedor <f.lushev@yandex.ru>
2023-03-30 12:43:56 +0700

    Добавлен тест на проверку существования мелодии по ее расположению

--- tests/tune_tests.py
+++ tests/tune_tests.py
@@ -10,3 +10,10 @@ class TestTune:
             path='tunes/test_tune.mp3'
         )
         assert tune is not None
+        return tune
+
+    def test_tune_check_exist(self):
+        tune = self.test_tune_create()
+        exist = tune.check_exist()
+        assert exist is True or exist is False
+
