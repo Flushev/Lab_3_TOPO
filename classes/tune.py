6c6cdd4f1602aeb75a43e6798c6b19e60ae4ac5b
Fedor <f.lushev@yandex.ru>
2023-03-30 12:49:52 +0700

    Добавлен метод проверки существования мелодии по ее расположению. Тест пройден

--- classes/tune.py
+++ classes/tune.py
@@ -1,9 +1,13 @@
+import os
+
 
 class Tune:
     name: str
     path: str
 
+    def check_exist(self):
+        return os.path.isfile(self.path)
+
     def __init__(self, name, path):
         self.name = name
         self.path = path
-
