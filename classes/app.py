cdbb0f7d8db8a52d328b94e521d67908046df4be
Fedor <f.lushev@yandex.ru>
2023-04-03 01:52:24 +0700

    Добавлен метод get_tune_list. Тест пройден

--- classes/app.py
+++ classes/app.py
@@ -1,8 +1,10 @@
 from .alarm import Alarm
+from .tune import Tune
 
 
 class App:
-    alarms: list[Alarm] | None
+    alarms: list[Alarm] | None = None
+    tunes: list[Tune] | None = None
 
     def add_alarm(self, alarm: Alarm):
         if self.alarms is None:
@@ -18,6 +20,16 @@ class App:
                 i += 1
         else:
             print("Список будильников пуст")
+        return True
+
+    def get_tune_list(self):
+        i = 1
+        if self.tunes is not None:
+            for tune in self.tunes:
+                print(f"{i}. {tune.name}")
+                i += 1
+        else:
+            print("Список рингтонов пуст")
         return True
 
     def __init__(self, alarms=None):
