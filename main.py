6cc67d79ec28da9d449727bcb9f8e86b8b4edd12
Fedor <f.lushev@yandex.ru>
2023-04-03 17:00:16 +0700

    Функции main.py переделаны под асинхронные, дописан метод создания будильника из консоли

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from classes.app import App
 from classes.alarm import Alarm
 from classes.tune import Tune
+import asyncio
 import datetime
 import os
 
@@ -27,7 +28,7 @@ def get_choice(values: [int]):
     return choice
 
 
-def main_page(app: App):
+async def main_page(app: App):
     os.system('cls')
     print("__Главное меню__\n")
     print("1. Список будильников")
@@ -36,12 +37,12 @@ def main_page(app: App):
     values = get_values_list(3)
     choice = get_choice(values)
     if choice == 1:
-        alarm_list(app)
+        await alarm_list(app)
     elif choice == 2:
-        create_alarm(app)
+        await create_alarm(app)
 
 
-def alarm_list(app: App):
+async def alarm_list(app: App):
     os.system('cls')
     print("__Список будильников__\n")
     app.get_alarm_list()
@@ -54,10 +55,10 @@ def alarm_list(app: App):
     choice = get_choice(values)
 
 
-def create_alarm(app: App):
+async def create_alarm(app: App):
     os.system('cls')
     print("__Добавление будильника__\n")
-    print("_Выбор рингтона_\n")
+    print("Выбор рингтона:")
     app.get_tune_list()
     if app.tunes is not None:
         l = app.tunes.__len__() + 1
@@ -67,10 +68,44 @@ def create_alarm(app: App):
     values = get_values_list(l)
     choice = get_choice(values)
     if choice == l:
-        create_tune(app)
+        await create_tune(app)
+    else:
+        tune = app.tunes[choice - 1]
+        print("Укажите время (часы): ", end='')
+        values = get_values_list(24)
+        hour = get_choice(values)
+        print("Укажите время (минуты): ", end='')
+        values = get_values_list(59)
+        minute = get_choice(values)
+        time_ = datetime.time(hour=hour, minute=minute)
+        print("Введите имя будильника: ", end='')
+        name = str(input())
+        print("Введите описание будильника: ", end='')
+        description = str(input())
+        print("Сделать будильник активным? (1 - да, 2 - нет): ", end='')
+        values = get_values_list(2)
+        active = get_choice(values)
+        if active == 1:
+            is_active = True
+        else:
+            is_active = False
+
+        alarm = Alarm(
+            name=name,
+            description=description,
+            tune=tune,
+            time=time_,
+            is_active=is_active
+        )
+
+        app.add_alarm(alarm)
+
+        print("Будильник успешно добавлен")
+        time.sleep(2)
+        await main_page(app)
 
 
-def create_tune(app: App):
+async def create_tune(app: App):
     os.system('cls')
     print("__Добавление рингтона__\n")
     print("Введите имя рингтона:")
@@ -85,18 +120,18 @@ def create_tune(app: App):
         print("Файл не найден. Попробуйте еще раз")
         time.sleep(2)
         del tune
-        create_alarm(app)
+        await create_alarm(app)
     else:
         app.add_tune(tune)
         print("Рингтон успешно добавлен")
         time.sleep(2)
-        create_alarm(app)
+        await create_alarm(app)
 
 
-def main():
+async def main():
     app = App()
-    main_page(app)
+    await main_page(app)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
